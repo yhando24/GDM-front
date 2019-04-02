4e1e8a04cdd1bd0a8a5edde4277805ff7bf14930
melanie <melanie.pichery@laposte.net>
2019-04-02 15:33:02 +0200

    ajout lien creation nature mission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreationNatureMissionComponent } from './creation-nature-mission/creation-nature-mission.component';
+
 
 
 
 export const ROUTES: Routes = [
+  { path: 'creationNature', component: CreationNatureMissionComponent },
   { path: '', pathMatch: 'full', redirectTo: 'accueil' }
 ];
