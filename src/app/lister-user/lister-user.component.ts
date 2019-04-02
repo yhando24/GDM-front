f5882bc2dd368ef5837b2ac190f1eccb86fdc1a6
mercadier yoann <yoann.mercadier@gmail.com>
2019-04-02 15:57:13 +0200

    delete user

--- src/app/lister-user/lister-user.component.ts
+++ src/app/lister-user/lister-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './../../services/user.service';
 import { User } from '../models';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -8,17 +9,39 @@ import { User } from '../models';
   templateUrl: './lister-user.component.html',
   styleUrls: ['./lister-user.component.css']
 })
-export class ListerUserComponent implements OnInit {
-  listeUsers;
-  constructor(private data: UserService) { }
-
-  ngOnInit() {
-     this.data.finAllUser()
-       .subscribe(arg => this.listeUsers = arg);
-  }
-  delete(user: User) {
-   /*  this.data.deleteOneUser(user)
-      .subscribe();
- */
-  }
+export class NgbdModalConfirmAutofocus {
+  constructor(public modal: NgbActiveModal) { }
 }
+export class NgbdModalConfirm {
+  constructor(public modal: NgbActiveModal) { }
+}
+const MODALS = {
+  focusFirst: NgbdModalConfirm,
+  autofocus: NgbdModalConfirmAutofocus
+};
+export class ListerUserComponent implements OnInit {
+         listeUsers;
+         withAutofocus = `<button type="button" ngbAutofocus class="btn btn-danger"
+      (click)="modal.close('Ok click')">Ok</button>`;
+         constructor(
+           private data: UserService,
+           private modalService: NgbModal
+         ) {}
+
+         ngOnInit() {
+           this.data
+             .finAllUser()
+             .subscribe(arg => (this.listeUsers = arg));
+         }
+         delete(user: User) {
+           /* this.data.deleteOneUser(user).subscribe(); */
+         }
+         update(user: User) {
+           /*  this.data.deleteOneUser(user)
+       .subscribe();
+  */
+         }
+         open(name: string) {
+           this.modalService.open(MODALS[name]);
+         }
+       }
