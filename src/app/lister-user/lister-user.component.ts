a1849fd59fdd223c3f6b425230d11aaa02cc25ca
mercadier yoann <yoann.mercadier@gmail.com>
2019-04-02 15:28:18 +0200

    change le nom dataservice en userservice

--- src/app/lister-user/lister-user.component.ts
+++ src/app/lister-user/lister-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from './../../services/data.service';
+import { UserService } from './../../services/user.service';
 import { User } from '../models';
 
 
