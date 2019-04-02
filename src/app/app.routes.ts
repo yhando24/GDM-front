be8f4bc774a6b9c9edb0f6b5c9d4ebcb1c7279bb
melanie <melanie.pichery@laposte.net>
2019-04-02 15:53:02 +0200

    création du composant accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CreationNatureMissionComponent } from './creation-nature-mission/creation-nature-mission.component';
 import { ListerUserComponent } from './lister-user/lister-user.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 export const ROUTES: Routes = [
+  { path: 'accueil', component: AccueilComponent },
   { path: 'creationNature', component: CreationNatureMissionComponent },
   { path: 'users', component: ListerUserComponent },
 
