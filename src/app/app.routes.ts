7222f1b6973b247397956f8f73a0078e6be08256
Mélanie D <44894678+Morue@users.noreply.github.com>
2019-04-03 10:27:27 +0200

    fix conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { ListerUserComponent } from './lister-user/lister-user.component';
 import { AccueilComponent } from './accueil/accueil.component';
 <<<<<<< HEAD
 import { UpdateNatureMissionComponent } from './update-nature-mission/update-nature-mission.component';
-=======
 import { FormusercreateComponent } from './formusercreate/formusercreate.component';
 >>>>>>> 1dad64d8ff2f7094c33e861efafb5cbc40954b91
 
@@ -17,7 +16,5 @@ export const ROUTES: Routes = [
   { path: 'updateNature', component: UpdateNatureMissionComponent},
 =======
   { path: 'creationUsers', component: FormusercreateComponent },
->>>>>>> 1dad64d8ff2f7094c33e861efafb5cbc40954b91
-
   { path: '', pathMatch: 'full', redirectTo: 'accueil' },
 ];
