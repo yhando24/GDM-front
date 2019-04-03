4c08b3364e571c707b52565618912e8aaad781f8
Delteil <a.delteil83@gmail.com>
2019-04-03 14:10:23 +0200

    correction bug list nature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,8 @@ import { ListerUserComponent } from './lister-user/lister-user.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AccueilComponent } from './accueil/accueil.component';
-<<<<<<< HEAD
 import { ListerNatureComponent } from './lister-nature/lister-nature.component';
-=======
-import { UpdateNatureMissionComponent } from './update-nature-mission/update-nature-mission.component';
-import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> bae29b225882e9b48d83a31d35e769611f5a7040
+
 
 
 @NgModule({
@@ -28,12 +24,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CreationNatureMissionComponent,
     HeaderComponent,
     AccueilComponent,
-<<<<<<< HEAD
     ListerNatureComponent],
-=======
-    UpdateNatureMissionComponent
-  ],
->>>>>>> bae29b225882e9b48d83a31d35e769611f5a7040
 
   imports: [
     BrowserModule,
@@ -41,7 +32,6 @@ import { ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     RouterModule.forRoot(ROUTES),
     FormsModule,
-    ReactiveFormsModule
   ],
 
   providers: [],
