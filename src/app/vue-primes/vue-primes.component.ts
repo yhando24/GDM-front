b2070bef2253a0f2ea3efe4df9331f0a5102c5e9
melanie <melanie.pichery@laposte.net>
2019-04-10 17:29:10 +0200

    mise en place vue primes

---
+++ src/app/vue-primes/vue-primes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MissionService } from 'src/services/mission.service';
+import { Mission, User } from '../models';
+import { UserService } from 'src/services/user.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-vue-primes',
+  templateUrl: './vue-primes.component.html',
+  styleUrls: ['./vue-primes.component.css']
+})
+export class VuePrimesComponent implements OnInit {
+
+  listeMissions: Mission[];
+
+  idUser: number;
+
+  constructor(private serviceMission: MissionService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+      this.idUser = Number(this.route.snapshot.paramMap.get('idUser'));
+
+      this.serviceMission.findMissionByUser(this.idUser).subscribe(
+      values => this.listeMissions = values,
+    );
+
+  }
+
+}
